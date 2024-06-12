cf617a061348fb3410ef26b861b6bd9669f6ab5c
JBKIM <drv98@naver.com>
2024-06-12 10:04:51 +0900

    카드섞기

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+
+//퍼블릭 폴더의 이미지들을 사용하기 쉽게 정리
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
+function App() {
+  //카드 배열을 스테이트로
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0); // 턴 횟수
+  //카드 섞기
+  const shuffleCards = () => {
+    const 랜덤카드 = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(랜덤카드);
+    setTurns(0);
+  };
+  console.log(cards, turns);
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
+}
+
+export default App;
