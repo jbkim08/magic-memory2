6dd59f1277afae38518add5868b66bf1efc22b1c
JBKIM <drv98@naver.com>
2024-06-12 10:37:25 +0900

    back이미지 클릭시 카드정보 출력

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -3,11 +3,19 @@ import "./SingleCard.css";
 
 //props 대신에 {프로퍼티이름}
 const SingleCard = ({ card }) => {
+  const handleClick = () => {
+    console.log(card);
+  };
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front"></img>
-        <img className="back" src="/img/cover.png" alt="card back"></img>
+        <img
+          onClick={handleClick}
+          className="back"
+          src="/img/cover.png"
+          alt="card back"
+        ></img>
       </div>
     </div>
   );
